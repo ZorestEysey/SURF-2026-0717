8d658be8cb6dd7720a92c2747042becfe708e953
ThisIsAName <senioreysey@163.com>
2026-08-04 10:47:35 +0800

    Refactor CSPF class to uniCSPF with new methods
    Added some methods to calculate left displacement, right displacement and net displacement.

--- CSPF.java
+++ CSPF.java
@@ -1,15 +1,13 @@
 import java.util.Arrays;
 
-public class CSPF {
-    static int n = 4;
-    static int index = 6;
+public class uniCSPF {
+
+    static int n = 3;
+    static int index = 1;
     static int carNum = 1;
     static int disp = 7;
 
-    static int[] pref = new int [n];
-
-    static void main (String[] args){
-
+    public static void main(String[] args) {
         // printAllCSPF();
 
         // printOneCSPF(index);
@@ -24,73 +22,213 @@ public class CSPF {
 
         // printCSPFByDisp(disp);
 
+        // System.out.println(computeLeftDisp(n));
+
+        // System.out.println(computeRightDisp(n));
+
+        // System.out.println(computeNetDisp(n));
+
+    }
+
+    public static int[] simulate(int[] pref) {
+        int n = pref.length;
+        boolean[] occupied = new boolean[n];
+        int[] finalPos = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int p = pref[i] - 1;
+            if (!occupied[p]) {
+                finalPos[i] = p;
+                occupied[p] = true;
+            } else {
+                int bestPos = -1;
+                int bestDist = Integer.MAX_VALUE;
+                for (int j = 0; j < n; j++) {
+                    if (!occupied[j]) {
+                        int dist = Math.abs(j - p);
+                        if (dist < bestDist || (dist == bestDist && j < bestPos)) {
+                            bestDist = dist;
+                            bestPos = j;
+                        }
+                    }
+                }
+                finalPos[i] = bestPos;
+                occupied[bestPos] = true;
+            }
+        }
+        return finalPos;
+    }
+
+    public static int calcTotalDisp(int[] pref) {
+        int[] finalPos = simulate(pref);
+        int total = 0;
+        for (int i = 0; i < pref.length; i++) {
+            total += Math.abs(finalPos[i] - (pref[i] - 1));
+        }
+        return total;
+    }
+
+    public static int[] calcLeftRight(int[] pref) {
+        int[] finalPos = simulate(pref);
+        int left = 0, right = 0;
+        for (int i = 0; i < pref.length; i++) {
+            int diff = finalPos[i] - (pref[i] - 1);
+            if (diff < 0) left += -diff;
+            else if (diff > 0) right += diff;
+        }
+        return new int[]{left, right};
+    }
+
+    public static int[] computeTotalDisp() {
+        checkN();
+        int totalConfigs = (int) Math.pow(n, n);
+        int maxTotalDisp = n * (n - 1) / 2;
+        int[] freq = new int[maxTotalDisp + 1];
+
+        for (int config = 0; config < totalConfigs; config++) {
+            int[] pref = intoArr(config);
+            int totalDisp = calcTotalDisp(pref);
+            freq[totalDisp]++;
+        }
+        return freq;
+    }
+
+    public static int computeLeftDisp(int n){
+        int[] totals = computeLeftRightTotals(n);
+        return totals[0];
+    }
+
+    public static int computeRightDisp(int n){
+        int[] totals = computeLeftRightTotals(n);
+        return totals[1];
+    }
+
+    public static int computeNetDisp(int n){
+        int[] totals = computeLeftRightTotals(n);
+        return totals[1]-totals[0];
+    }
+
+    public static int[] computeLeftRightTotals(int n) {
+        checkN();
+        int totalLeft = 0, totalRight = 0;
+        int totalConfigs = (int) Math.pow(n, n);
+
+        for (int config = 0; config < totalConfigs; config++) {
+            int[] pref = intoArr(config);
+            int[] lr = calcLeftRight(pref);
+            totalLeft += lr[0];
+            totalRight += lr[1];
+        }
+        return new int[]{totalLeft, totalRight};
+    }
+
+    public static int getDisp(int index, int carNum) {
+        checkN();
+        checkIndex(index);
+        checkCarNum(carNum);
+
+        int[] pref = intoArr(index - 1);
+        int[] finalPos = simulate(pref);
+        return finalPos[carNum - 1] - (pref[carNum - 1] - 1);
+    }
+
+    public static int getLeftDisp(int n) {
+        return computeLeftRightTotals(n)[0];
+    }
+
+    public static int getRightDisp(int n) {
+        return computeLeftRightTotals(n)[1];
+    }
+
+    public static int getNetDisp(int n) {
+        int[] totals = computeLeftRightTotals(n);
+        return totals[0] - totals[1];
+    }
+
+    public static void printAllCSPF() {
+        checkN();
+        int total = (int) Math.pow(n, n);
+        for (int i = 0; i < total; i++) {
+            int[] pref = intoArr(i);
+            int totalDisp = calcTotalDisp(pref);
+            System.out.print((i + 1) + ". " + Arrays.toString(pref) + " -> D=" + totalDisp);
+            System.out.println();
+        }
+    }
+
+    public static void printOneCSPF(int index) {
+        checkIndex(index);
+        int[] pref = intoArr(index - 1);
+        System.out.print(index + ". " + Arrays.toString(pref));
+        System.out.println();
+    }
+
+    public static void printCSPFByDisp(int disp) {
+        checkSingleDisp(disp);
+        int totalConfigs = (int) Math.pow(n, n);
+        for (int idx = 0; idx < totalConfigs; idx++) {
+            int[] pref = intoArr(idx);
+            int totalDisp = calcTotalDisp(pref);
+            if (totalDisp == disp) {
+                System.out.println((idx + 1) + ". " + Arrays.toString(pref));
+            }
+        }
     }
 
     public static boolean isUniCSPF(int index) {
         for (int k = 1; k <= n; k++) {
-            if (getDisp(index, k) > 1) {
+            if (Math.abs(getDisp(index, k)) > 1) {
                 return false;
             }
         }
         return true;
     }
 
-    public static void checkN(){
-        if (n <= 0) {
-            throw new IllegalStateException("n should be an positive integer.");
-        }
-    }
-
-    public static void checkIndex(){
-        long maxIndex = (long) Math.pow(n, n);
-        if (index < 1 || index > maxIndex) {
-            throw new IllegalArgumentException("Your index should between 1 and " + maxIndex + ".");
-        }
-    }
-
-    public static void checkCarNum(){
-        if (carNum < 1 || carNum > n) {
-            throw new IllegalArgumentException("Your carNum should between 1 and " + n +".");
-        }
-    }
-
-    public static void checkSingleDisp(){
-        if (disp < 0 || disp > n-1){
-            throw new IllegalArgumentException("Your disp should between 0 and " + (n-1) +".");
-        }
-    }
     public static void printUniCSPF() {
         checkN();
         int total = (int) Math.pow(n, n);
         for (int i = 1; i <= total; i++) {
             if (isUniCSPF(i)) {
-                System.out.println(i + ". " + Arrays.toString(intoArr(i-1)));
+                System.out.println(i + ". " + Arrays.toString(intoArr(i - 1)));
             }
         }
     }
 
-    public static int countUniCSPF(){
+    public static int countUniCSPF() {
         checkN();
-        int totalUnis = 0;
-        for (int i = 1; i <= (int) Math.pow(n, n); i++){
-            if (isUniCSPF(i)){
-                totalUnis++;
-            }
+        int count = 0;
+        int total = (int) Math.pow(n, n);
+        for (int i = 1; i <= total; i++) {
+            if (isUniCSPF(i)) count++;
         }
-        return totalUnis;
+        return count;
     }
-    public static void printAllCSPF(){
-        checkN();
-        for (int i = 0; i < (int) Math.pow(n, n); i++){
-            printOneCSPF(i+1);
+
+    public static void checkN() {
+        if (n <= 0) {
+            throw new IllegalStateException("n must be a positive integer.");
         }
     }
 
-    public static void printOneCSPF (int index){
-        checkIndex();
-        System.out.print(index + ". " + Arrays.toString(intoArr(index-1)));
-        System.out.println();
+    public static void checkIndex(int index) {
+        long maxIndex = (long) Math.pow(n, n);
+        if (index < 1 || index > maxIndex) {
+            throw new IllegalArgumentException("Index must be between 1 and " + maxIndex + ".");
+        }
     }
+
+    public static void checkCarNum(int carNum) {
+        if (carNum < 1 || carNum > n) {
+            throw new IllegalArgumentException("carNum must be between 1 and " + n + ".");
+        }
+    }
+
+    public static void checkSingleDisp(int disp) {
+        if (disp < 0 || disp > n - 1) {
+            throw new IllegalArgumentException("disp must be between 0 and " + (n - 1) + ".");
+        }
+    }
+
 
     public static int[] intoArr(int index) {
         int[] arr = new int[n];
@@ -98,114 +236,5 @@ public class CSPF {
             arr[n - 1 - j] = (index / (int) Math.pow(n, j)) % n + 1;
         }
         return arr;
-    }
-
-    public static int getDisp(int index, int carNum) {
-        checkN();
-        checkIndex();
-        checkCarNum();
-
-        pref = intoArr(index - 1);
-        int[] path = new int[pref.length];
-
-        for (int i = 0; i < carNum - 1; i++) {
-            int prefer = pref[i] - 1;
-            if (path[prefer] == 0) {
-                path[prefer] = i + 1;
-            } else {
-                int left = prefer - 1, right = prefer + 1;
-                while (true) {
-                    boolean leftOk = left >= 0 && path[left] == 0;
-                    boolean rightOk = right < path.length && path[right] == 0;
-                    if (leftOk) {
-                        path[left] = i + 1;
-                        break;
-                    } else if (rightOk) {
-                        path[right] = i + 1;
-                        break;
-                    }
-                    left--;
-                    right++;
-                }
-            }
-        }
-
-        int prefer = pref[carNum - 1] - 1;
-        if (path[prefer] == 0) {
-            return 0;
-        } else {
-            int left = prefer - 1, right = prefer + 1;
-            while (true) {
-                boolean leftOk = left >= 0 && path[left] == 0;
-                boolean rightOk = right < path.length && path[right] == 0;
-                if (leftOk) {
-                    return prefer - left;
-                } else if (rightOk) {
-                    return right - prefer;
-                }
-                left--;
-                right++;
-            }
-        }
-    }
-
-    public static int[] computeTotalDisp() {
-        checkN();
-
-        int totalConfigs = (int) Math.pow(n, n);
-        int maxTotalDisp = (n * (n - 1))/2;
-        int[] freq = new int[maxTotalDisp + 1];
-
-        for (int config = 0; config < totalConfigs; config++) {
-            int[] pref = intoArr(config);
-            int totalDisp = calcTotalDispHelper(pref);
-            freq[totalDisp]++;
-        }
-        return freq;
-    }
-
-    private static int calcTotalDispHelper(int[] pref) {
-        int[] path = new int[n];
-        int totalDisp = 0;
-
-        for (int i = 0; i < n; i++) {
-            int prefer = pref[i] - 1;
-            if (path[prefer] == 0) {
-                path[prefer] = i + 1;
-            } else {
-                int left = prefer - 1, right = prefer + 1;
-                int dist = 1;
-                while (true) {
-                    boolean leftOk = left >= 0 && path[left] == 0;
-                    boolean rightOk = right < n && path[right] == 0;
-                    if (leftOk) {
-                        path[left] = i + 1;
-                        totalDisp += dist;
-                        break;
-                    } else if (rightOk) {
-                        path[right] = i + 1;
-                        totalDisp += dist;
-                        break;
-                    }
-                    left--;
-                    right++;
-                    dist++;
-                }
-            }
-        }
-        return totalDisp;
-    }
-
-    public static void printCSPFByDisp(int disp) {
-        checkSingleDisp();
-
-        int totalConfigs = (int) Math.pow(n, n);
-        for (int index = 0; index < totalConfigs; index++) {
-            int[] pref = intoArr(index);
-            int totalDisp = calcTotalDispHelper(pref);
-            if (totalDisp == disp) {
-                System.out.println((index+1)+". "+Arrays.toString(pref));
-            }
-        }
     }
 }
